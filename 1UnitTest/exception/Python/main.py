e644b3d090acdd7eccff8469d20e674dbd6116a8
Панюшкин Павел <Parframe@gmail.com>
2021-09-14 11:40:25 +0300

    Update

--- 1UnitTest/exception/Python/main.py
+++ 1UnitTest/exception/Python/main.py
@@ -5,12 +5,13 @@ try:
     num1 = float(input())
 
     print("Enter second number")
-    num2 = float(input(input))
+    num2 = float(input())
 
     res = num1 / num2
+    print("Result: " + str(res))
 except Exception as e:
     print("Error! " + str(e))
 
 
-print(sys.getsizeof(int))
-print(sys.getsizeof(float))
+print("Size of integer: " + str(sys.getsizeof(int())))
+print("Size of float: " + str(sys.getsizeof(float())))
